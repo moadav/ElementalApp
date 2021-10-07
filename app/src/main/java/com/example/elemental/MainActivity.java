eba2e5bb9de66e35ef1fee9aff3fcdcf984e6168
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-10-07 17:43:35 +0200

    ordnet Toolbar

--- app/src/main/java/com/example/elemental/MainActivity.java
+++ app/src/main/java/com/example/elemental/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.elemental;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.main_Toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
 
-    //TEST COMMIT
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
 }
