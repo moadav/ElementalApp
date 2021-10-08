18c4a67fcbea9c32fe3c5b66020ffba0f3573f84
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-10-08 19:42:38 +0200

    Fikset sport,bmi og kalender fragments og navigasjon som linker seg mellom de ulike fragmentene

--- app/src/main/java/com/example/elemental/MainActivity.java
+++ app/src/main/java/com/example/elemental/MainActivity.java
@@ -1,15 +1,34 @@
 package com.example.elemental;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.example.elemental.Fragments.BMIKalkulatorFragment;
+import com.example.elemental.Fragments.CalendarFragment;
+import com.example.elemental.Fragments.SportTipsFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationBarView.OnItemSelectedListener {
 
     private Toolbar toolbar;
+    private BMIKalkulatorFragment bmiKalkulatorFragment = new BMIKalkulatorFragment();
+    private CalendarFragment calendarFragment = new CalendarFragment();
+    private SportTipsFragment sportTipsFragment = new SportTipsFragment();
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +37,31 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.main_Toolbar);
         setSupportActionBar(toolbar);
+        //replaceFragment(calendarFragment);
 
+        bottomNavigation = (BottomNavigationView) findViewById(R.id.Bottom_navigation);
+
+        bottomNavigation.setOnItemSelectedListener(this);
+
+
+
+    }
+    private void replaceFragment(Fragment fragment){
+
+
+
+
+
+
+
+
+
+        if(fragment != null){
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.Nav_container, fragment);
+            fragmentTransaction.commit();
+
+        }
     }
 
     @Override
@@ -26,5 +69,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreateOptionsMenu(menu);
         getMenuInflater().inflate(R.menu.main_menu,menu);
         return true;
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.sport_check:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Nav_container,sportTipsFragment).commit();
+                Log.d("asdasdasda","DICK DICK DICK");
+                return true;
+
+            case R.id.calendar_check:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Nav_container,calendarFragment).commit();
+                Log.d("asdasdasda","DICK DICK DICK");
+
+                return true;
+            case R.id.bmi_check:
+                getSupportFragmentManager().beginTransaction().replace(R.id.Nav_container,bmiKalkulatorFragment).commit();
+                Log.d("asdasdasda","DICK DICK DICK");
+                return true;
+
+        }
+
+        return false;
     }
 }
