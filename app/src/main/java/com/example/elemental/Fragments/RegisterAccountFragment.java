1a6a17081b4132735b57348986e75404a1bb698f
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-10-22 23:14:32 +0200

    Firebase integrasjon ferdig, lagrer nødvendig verdier

--- app/src/main/java/com/example/elemental/Fragments/RegisterAccountFragment.java
+++ app/src/main/java/com/example/elemental/Fragments/RegisterAccountFragment.java
@@ -2,16 +2,29 @@ package com.example.elemental.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Patterns;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.elemental.R;
+import com.example.elemental.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,19 +42,19 @@ public class RegisterAccountFragment extends Fragment implements View.OnClickLis
     private String mParam1;
     private String mParam2;
 
+
+    private EditText username, password, email, height,weight;
+    private ProgressBar progresscircle;
+    private Button registerbutton;
+    private ImageView imageView2;
+
+    private FirebaseAuth mAuth;
+
+
     public RegisterAccountFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterAccountFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static RegisterAccountFragment newInstance(String param1, String param2) {
         RegisterAccountFragment fragment = new RegisterAccountFragment();
         Bundle args = new Bundle();
@@ -75,8 +88,26 @@ public class RegisterAccountFragment extends Fragment implements View.OnClickLis
     public void onResume() {
         super.onResume();
 
-        ImageView imageView = (ImageView) getView().findViewById(R.id.imageView2);
-        imageView.setOnClickListener(this);
+        imageView2 = (ImageView) getView().findViewById(R.id.imageView2);
+        imageView2.setOnClickListener(this);
+
+        registerbutton = (Button) getView().findViewById(R.id.registerbutton);
+        registerbutton.setOnClickListener(this);
+
+        username = (EditText) getView().findViewById(R.id.username);
+
+        password = (EditText) getView().findViewById(R.id.password);
+
+        email = (EditText) getView().findViewById(R.id.email);
+
+        height = (EditText) getView().findViewById(R.id.height);
+
+        weight = (EditText) getView().findViewById(R.id.weight);
+
+        progresscircle = (ProgressBar) getView().findViewById(R.id.progresscircle);
+
+        mAuth = FirebaseAuth.getInstance();
+
 
     }
 
@@ -86,8 +117,86 @@ public class RegisterAccountFragment extends Fragment implements View.OnClickLis
         switch (view.getId()){
             case R.id.imageView2:
                 Navigation.findNavController(view).navigate(R.id.action_registerAccountFragment_to_loginFragment);
-
                 break;
+            case R.id.registerbutton:
+                registerUser();
+                break;
+
         }
+    }
+
+    private void registerUser() {
+
+        String emailText = email.getText().toString().trim();
+        String usernameText = username.getText().toString().trim();
+        String weightText = weight.getText().toString().trim();
+        String heightText = height.getText().toString().trim();
+        String passwordText = password.getText().toString().trim();
+
+        if(emailText.isEmpty()){
+            email.setError("Email is required");
+            email.requestFocus();
+            return;
+        }
+        if(usernameText.isEmpty()){
+            username.setError("Username is required");
+            username.requestFocus();
+            return;
+        }
+        if(weightText.isEmpty()){
+            weight.setError("Weight is required");
+            weight.requestFocus();
+            return;
+        }
+        if(heightText.isEmpty()){
+            height.setError("Height is required");
+            height.requestFocus();
+            return;
+        }
+        if(passwordText.isEmpty()){
+            password.setError("Password is required");
+            password.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(emailText).matches()){
+            email.setError("Please provide a valid email");
+            email.requestFocus();
+            return;
+        }
+
+        if(passwordText.length() < 6){
+            password.setError("Password should have atleast 6 characters!");
+            password.requestFocus();
+            return;
+        }
+
+        progresscircle.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(emailText,passwordText)
+        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                if(task.isSuccessful()){
+
+                    User user = new User(weightText,heightText,usernameText,emailText);
+
+                    FirebaseDatabase.getInstance().getReference("Users").child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if(task.isSuccessful()){
+                                Toast.makeText(getActivity(), "User has been registered!", Toast.LENGTH_LONG).show();
+                                progresscircle.setVisibility(View.GONE);
+                            }else{
+                                Toast.makeText(getActivity(), "Failed to register! Please try again", Toast.LENGTH_LONG).show();
+                                progresscircle.setVisibility(View.GONE);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/elemental/User.java
@@ -0,0 +1,20 @@
+package com.example.elemental;
+
+public class User {
+
+public String weight, height, username, email;
+
+
+public User(String weight, String height, String username, String email){
+
+    this.weight = weight;
+    this.height = height;
+    this.username = username;
+    this.email = email;
+}
+public User(){
+
+}
+
+
+}
