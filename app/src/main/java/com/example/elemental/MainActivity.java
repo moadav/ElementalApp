6d1b866d5074ecd4598c83f1fcf75b2cab30fea6
Mohammed Ali Davami <69807363+moadav@users.noreply.github.com>
2021-11-05 06:54:50 +0100

    navigation drawer. Men er buggy

--- app/src/main/java/com/example/elemental/MainActivity.java
+++ app/src/main/java/com/example/elemental/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.elemental;
 
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
@@ -20,6 +22,7 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.core.View;
 
@@ -27,6 +30,67 @@ import com.google.firebase.firestore.core.View;
 public class MainActivity extends AppCompatActivity  {
 
     private Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle actionBarToggle;
+
+    /*
+       <androidx.drawerlayout.widget.DrawerLayout
+    android:id="@+id/drawerLayout"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    tools:openDrawer="start">
+
+    <androidx.coordinatorlayout.widget.CoordinatorLayout
+        android:layout_width="match_parent"
+        android:layout_height="match_parent"
+        android:fitsSystemWindows="true">
+
+        <com.google.android.material.appbar.AppBarLayout
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"
+            style="@style/Widget.MaterialComponents.AppBarLayout.PrimarySurface"
+            android:fitsSystemWindows="true">
+
+            <com.google.android.material.appbar.MaterialToolbar
+                android:id="@+id/topAppBar"
+                android:layout_width="match_parent"
+                android:layout_height="?attr/actionBarSize"
+                app:title="ttt"
+                app:navigationIcon="@drawable/ic_baseline_menu_24"
+                style="@style/Widget.MaterialComponents.Toolbar.PrimarySurface"
+                android:background="@android:color/transparent"
+                android:elevation="0dp" />
+
+        </com.google.android.material.appbar.AppBarLayout>
+
+        <!-- Screen content -->
+        <!-- Use app:layout_behavior="@string/appbar_scrolling_view_behavior" to fit below top app bar -->
+
+    </androidx.coordinatorlayout.widget.CoordinatorLayout>
+
+
+
+</androidx.drawerlayout.widget.DrawerLayout>
+
+
+ <com.google.android.material.appbar.MaterialToolbar
+                android:id="@+id/topAppBar"
+                android:layout_width="match_parent"
+                android:layout_height="?attr/actionBarSize"
+                app:title="ttt"
+
+                />
+
+<com.google.android.material.appbar.MaterialToolbar
+                android:id="@+id/topAppBar"
+                android:layout_width="match_parent"
+                android:layout_height="?attr/actionBarSize"
+                app:title="ttt"
+                app:navigationIcon="@drawable/ic_baseline_menu_24"
+                style="@style/Widget.MaterialComponents.Toolbar.PrimarySurface"
+                android:background="@android:color/transparent"
+                android:elevation="0dp" />
+     */
 
 
     @Override
@@ -36,14 +100,53 @@ public class MainActivity extends AppCompatActivity  {
 
         toolbar = findViewById(R.id.main_Toolbar);
         setSupportActionBar(toolbar);
-        
+
+
+
         BottomNavigationView bottomNavigationView = findViewById(R.id.Bottom_navigation);
         NavController navController = Navigation.findNavController(this,  R.id.Nav_container);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupActionBarWithNavController(this, navController);
         getSupportActionBar().setTitle("Elemental");
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.myDrawerMenu);
+
+
+        actionBarToggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.open_drawer,R.string.close_drawer);
+        actionBarToggle.setDrawerIndicatorEnabled(true);
+        drawerLayout.addDrawerListener(actionBarToggle);
+        actionBarToggle.syncState();
+        NavigationView navigationView = findViewById(R.id.navigview);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.item:
+                        Toast.makeText(getApplicationContext(), "Failed to register! Please try again", Toast.LENGTH_LONG).show();
+                        break;
+
+                }
+
+
+
+                return false;
+            }
+        });
+
+
+
+
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -64,7 +167,7 @@ public class MainActivity extends AppCompatActivity  {
 
                 break;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item) || actionBarToggle.onOptionsItemSelected(item);
 
     }
 
